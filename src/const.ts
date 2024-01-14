0dfd70b9521feb10bcf69e284d86bcda76e7a457
Zita Kammermayerova <zitakammermayerova@gmail.com>
2024-01-14 17:29:15 +0100

    Add introduction post

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,10 @@ export const SITE_TITLE = "Zero Deflection";
 
 export const BLOG_TITLE = `${SITE_TITLE.toUpperCase()} Blog`;
 export const BLOG_DESCRIPTION = "A coding blogposts";
+
+export const GITHUB_DAN_URL = "https://github.com/danad02";
+export const GITHUB_ZERO_DEFLECTION_URL = "https://github.com/zero-deflection";
+export const GITHUB_ZITA_URL = "https://github.com/zitak";
+
+export const LINKEDIN_DAN_URL = "https://www.linkedin.com/in/danad02/";
+export const LINKEDIN_ZITA_URL = "https://www.linkedin.com/in/zitak/";
