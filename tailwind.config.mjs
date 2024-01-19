818ffe3ed178db44e08e0eda9fd371126ea27b84
Zita Kammermayerová <ZitaKammermayerova@gmail.com>
2024-01-19 17:11:35 +0100

    Add introduction post (#9)
    * Add introduction post
    
    * feat: improved mdx elements
    
    * feat: add H2 component
    
    * feat: add import alias
    
    ---------
    
    Co-authored-by: Dan Polansky <polansky.dan@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      "light-gray": "#F7F2E7",
-      "medium-gray": "#D8D3CD",
-      "dark-gray": "#797A7E",
-      "light-green": "#EFF5F5",
-      "medium-green": "#D6E4E5",
-      "dark-green": "#497174",
-      "ultra-dark-green": "#00303F",
-      brown: "#CDAC81",
-      violet: "#991A68",
-    },
     extend: {
+      colors: {
+        "light-gray": "#F7F2E7",
+        "medium-gray": "#D8D3CD",
+        "dark-gray": "#797A7E",
+        "light-green": "#EFF5F5",
+        "medium-green": "#D6E4E5",
+        "dark-green": "#497174",
+        "ultra-dark-green": "#00303F",
+        brown: "#CDAC81",
+        violet: "#991A68",
+      },
       gridTemplateColumns: {
         "blog-posts": "repeat(auto-fit, minmax(24ch, 1fr))",
       },
